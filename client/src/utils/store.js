4a755b7c13f4c8bf19ed24f4fa858da1cf60ab3d
Thomas Bolling <Thomas.Bolling.87@gmail.com>
2021-08-07 16:59:37 -0500

    deleted the globalstate and created the store to replace it

---
+++ client/src/utils/store.js
@@ -0,0 +1,12 @@
+import { createStore } from "redux";
+import { reducers } from "./reducers";
+
+const initialState = {
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: "",
+};
+
+export default createStore(reducers, initialState);
